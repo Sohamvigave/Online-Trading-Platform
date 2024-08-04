a7db0785c702252ce6e00bf5648c519b1ae4f631
soham vigave <sohamvigave@gmail.com>
2024-08-04 21:18:53 +0530

    setting up support-component

--- src/landing_page/support/SupportPage.js
+++ src/landing_page/support/SupportPage.js
@@ -1,16 +1,35 @@
-import React from 'react';
-import CreateTicket from './CreateTicket';
-import Hero from './Hero';
-import Navbar from '../Navbar';
-import Footer from '../Footer';
+// import React from 'react';
+// import CreateTicket from './CreateTicket';
+// import Hero from './Hero';
+// import Navbar from '../Navbar';
+// import Footer from '../Footer';
 
-function SupportPage() {
+// function SupportPage() {
+//     return (
+//         <>
+//             <CreateTicket />
+//             <Hero />
+//         </>
+//     );
+// }
+
+// export default SupportPage;
+
+import React from "react";
+
+import Hero from "./Hero";
+import CreateTicket from "./CreateTicket";
+
+import Navbar from "../Navbar";
+import Footer from "../Footer";
+
+function PricingPage() {
     return (
         <>
-            <CreateTicket />
             <Hero />
+            <CreateTicket />
         </>
     );
 }
 
-export default SupportPage;
+export default PricingPage;
