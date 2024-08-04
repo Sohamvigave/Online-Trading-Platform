f3da63f2dc87eccb80683c4f47bde778e2288816
soham vigave <sohamvigave@gmail.com>
2024-08-04 14:25:16 +0530

    setting up react-router

---
+++ src/landing_page/pricing/PricingPage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Pricing from "./Pricing";
+import Brokerage from "./Brokerage";
+import Navbar from "../Navbar";
+import Footer from "../Footer";
+
+function PricingPage() {
+    return (
+        <>
+            <Pricing />
+            <Brokerage />
+        </>
+    );
+}
+
+export default PricingPage;
