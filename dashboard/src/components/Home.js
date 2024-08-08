841be8c98e24d453ec3792b2583eca1f17a81caf
soham vigave <sohamvigave@gmail.com>
2024-08-08 10:40:42 +0530

    adding dashboard and frontend

---
+++ dashboard/src/components/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import TopBar from "./Topbar";
+import Dashboard from "./Dashboard";
+
+const Home = () => {
+    return (
+        <>
+            <TopBar />
+            <Dashboard />
+        </>
+    );
+};
+
+export default Home;
